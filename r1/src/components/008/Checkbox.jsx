d61d54c26e60232e94db9c82a1c637b3022a3ee3
rollen1 <nolovedivision@gmail.com>
2022-09-01 20:02:55 +0300

    cb

--- r1/src/components/008/Checkbox.jsx
+++ r1/src/components/008/Checkbox.jsx
@@ -16,19 +16,19 @@ function Checkbox() {
             <div className="form-container">
                 <div>
                     <input type="checkbox" value="A" id="_1" onChange={change} checked={cb.A}></input>
-                    <label htmlFor="_1">A Raidė</label>
+                    <label htmlFor="_1" style={{color: cb.A ? 'green' : null}}>A Raidė</label>
                 </div>
                 <div>
                     <input type="checkbox" value="B" id="_2" onChange={change} checked={cb.B}></input>
-                    <label htmlFor="_2">B Raidė</label>
+                    <label htmlFor="_2" style={{color: cb.B ? 'green' : null}}>B Raidė</label>
                 </div>
                 <div>
                     <input type="checkbox" value="C" id="_3" onChange={change} checked={cb.C}></input>
-                    <label htmlFor="_3">C Raidė</label>
+                    <label htmlFor="_3" style={{color: cb.C ? 'green' : null}}>C Raidė</label>
                 </div>
                 <div>
                     <input type="checkbox" value="D" id="_4" onChange={change} checked={cb.D}></input>
-                    <label htmlFor="_4">D Raidė</label>
+                    <label htmlFor="_4" style={{color: cb.D ? 'green' : null}}>D Raidė</label>
                 </div>
 
             </div>
